05688bad8e9dd9f07676e08e8767ae3fb1d77932
MamutoKing <cameron.churchillmedina@csuglobal.edu>
2026-06-05 19:11:07 -0600

    Second commit

--- TaxRate.java
+++ TaxRate.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class TaxRate {
     public static void main (String[] args) {
@@ -9,39 +10,56 @@ public class TaxRate {
 
         //Variables
         double weeklyIncome;
-        double taxRate;
+        double taxRate = 0;
         double taxAmount;
+        int bracket;
 
-
+        try{
         // Prompt user for Income
-        System.out.println("Enter your weekly income: ");
+            System.out.println("Enter your weekly income: ");
 
-        weeklyIncome = scnr.nextDouble();
+            weeklyIncome = scnr.nextDouble();
 
+        //Throw for invalid (negative) income
+            if (weeklyIncome < 0) {
+                throw new IllegalArgumentException ("income cannot be negative.");
+            }
 
         //If-Else tree to determine Tax Rate
-        if (weeklyIncome < 500) {
-            taxRate = 0.10;
-        }
+            if (weeklyIncome < 500) {
+                taxRate = 0.10;
+            }
 
-        else if (weeklyIncome >= 500 && weeklyIncome < 1500) {
-            taxRate = 0.15;
-        }
+            else if (weeklyIncome >= 500 && weeklyIncome < 1500) {
+                taxRate = 0.15;
+            }
 
-        else if (weeklyIncome >= 1500 && weeklyIncome < 2500) {
-            taxRate = 0.20;
-        }
+            else if (weeklyIncome >= 1500 && weeklyIncome < 2500) {
+                taxRate = 0.20;
+            }
 
-        else {
-            taxRate = 0.30;
-        }
+            else {
+                taxRate = 0.30;
+            }
 
         //Calculate tax amount
-        taxAmount = weeklyIncome * taxRate;
+            taxAmount = weeklyIncome * taxRate;
 
         //Display tax amount up to two decimal places
-        System.out.printf("The weekly tax withholding is: $%.2f",  taxAmount);
+            System.out.printf("The weekly tax withholding is: $%.2f",  taxAmount);
+        }
 
-        scnr.close();
+        catch (inputMismatchException e){
+            System.out.println ("Error: Please enter a valid numeric value for income.");
+        }
+
+        catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        finally {
+            scnr.close();
+            System.out.println("Program closed.");
+        }
     }
 }
